feea01ca186a359a648fa4c816e064e1bb1d278a
Yevgen <109423506+jekahomie@users.noreply.github.com>
2022-07-16 17:40:57 +0200

    Add files via upload
    Fix mistakes + add exercise №2

--- Homework2.py
+++ Homework2.py
@@ -7,23 +7,62 @@ age = int(input("How old are you ? :"))
 if age == 16:
     print(name, ", you need to wait one year more")
 
-
 elif 17<=age<71:
     print("Welcome,", name, ", on our website!")
-
 
 elif 71<=age<91:
     print("You are lucky,", name, ", and welcome!")
 
-
 elif 91<=age<121:
     print(name, ", are you sure?")
-
 
 elif age >= 121:
     print("You are not real,", name, ".")
 
 else:
-    print("I'm so sorry,", name, ", you are too young! wait a few years more",".")
+    print("I'm so sorry,", name, ", you`re too young!wait a few years more",".")
 
 
+#Задание №2
+
+#Присваивание (=)
+3 = 4
+
+#Сложение(+)
+3 + 2
+
+#Вычитание(-)
+3 - 2
+
+#Умножение(*)
+3 * 2
+
+#Деление (/)
+3 / 2
+
+#Возведение в степень (**)
+3 ** 4
+
+#Деление без остатка(//) - убирает числа после точки(float to int)
+3 // 4
+
+#Деление по модулю(%)
+3 % 4
+
+#Больше (>)
+3 > 4
+
+#Меньше (<)
+3 < 4
+
+#Больше или равно (>=)
+3 >= 4
+
+#Меньше или равно (<=)
+3 <= 4
+
+#Равно (==)
+3 == 4
+
+#Не равно (!=)
+3 != 4
